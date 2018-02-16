90ca50e191bb3589256e67fa18f45f8f998a2562
tinawh <tinawhuang95@gmail.com>
2018-02-16 11:54:08 -0500

    ran hypermodules_1000

--- 8_hypermodules_100.sh
+++ 8_hypermodules_100.sh
@@ -3,5 +3,5 @@
 cd /.mounts/labs/reimandlab/private/users/thuang/bin_2/hypermodules_100/
 
 for file in *.sh; do 
-	qsub -cwd -V -l h_vmem=9G -pe smp 9- $file
+	qsub -cwd -V -l h_vmem=70G $file
 done
